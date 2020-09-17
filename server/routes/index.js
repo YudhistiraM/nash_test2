52d96901209da08349107fe7a50afb3ae8bf276d
yudhies2109 <ma.yudhistira@hotmail.com>
2020-09-17 21:54:58 +0700

    Get data from database

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+module.exports = function (pool) {
+
+  /* GET home page. */
+  router.get('/', function (req, res, next) {
+    try {
+      let sql = `SELECT * FROM mahasiswa`;
+      pool.query(sql, (err, data) => {
+        res.json(data);
+        console.log('ini data', data);
+      })
+    } catch (err) {
+      res.json({
+        error: true,
+        message: `something went wrong : ${err.message}`
+      })
+    }
+
+  });
+
+  return router;
+}
