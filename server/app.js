e6e77042701c97ed8626b671221fac64d13a05da
yudhies2109 <ma.yudhistira@hotmail.com>
2020-09-18 16:42:18 +0700

    cors install server

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ const pool = new Pool ({
 
 var indexRouter = require('./routes/index')(pool);
 var usersRouter = require('./routes/users');
+let cors = require('cors');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
