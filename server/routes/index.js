43bd504552f7bac2a6220019dc25bfabbbac93e9
yudhies2109 <ma.yudhistira@hotmail.com>
2020-09-18 08:00:26 +0700

    Server Show & delete data

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,13 @@ module.exports = function (pool) {
   /* GET home page data mahasiswa. */
   router.get('/', function (req, res, next) {
     try {
-      let sql = `SELECT * FROM mahasiswa`;
+      let sql = `SELECT m.id_mhs, m.nama_mhs, mt.nama_matkul, nilai_mhs FROM mahasiswa as m, mata_kuliah as mt, nilai WHERE m.id_mhs = nilai.id_mhs AND mt.id_matkul = nilai.id_matkul;`;
       pool.query(sql, (err, data) => {
-        res.json(data.rows);
-        // console.log('ini data', data.rows);
+        res.json({
+          success: true,
+          message: "Data has been load successfully",
+          data: data.rows
+        });
       })
     } catch (err) {
       res.json({
@@ -18,28 +21,76 @@ module.exports = function (pool) {
         message: `something went wrong : ${err.message}`
       })
     }
+  });
 
+  router.get('/mahasiswa', function (req, res, next) {
+    try {
+      let sql = `SELECT * FROM mahasiswa;`;
+      pool.query(sql, (err, data) => {
+        res.json({
+          success: true,
+          message: "Data has been load successfully",
+          data: data.rows
+        });
+      })
+    } catch (err) {
+      res.json({
+        error: true,
+        message: `something went wrong : ${err.message}`
+      })
+    }
+  });
+
+  router.get('/mata_kuliah', function (req, res, next) {
+    try {
+      let sql = `SELECT * FROM mata_kuliah;`;
+      pool.query(sql, (err, data) => {
+        res.json({
+          success: true,
+          message: "Data has been load successfully",
+          data: data.rows
+        });
+      })
+    } catch (err) {
+      res.json({
+        error: true,
+        message: `something went wrong : ${err.message}`
+      })
+    }
+  });
+
+  router.get('/', function (req, res, next) {
+    try {
+      let sql = `SELECT m.id_mhs, m.nama_mhs, mt.nama_matkul, nilai_mhs FROM mahasiswa as m, mata_kuliah as mt, nilai WHERE m.id_mhs = nilai.id_mhs AND mt.id_matkul = nilai.id_matkul;`;
+      pool.query(sql, (err, data) => {
+        res.json({
+          success: true,
+          message: "Data has been load successfully",
+          data: data.rows
+        });
+      })
+    } catch (err) {
+      res.json({
+        error: true,
+        message: `something went wrong : ${err.message}`
+      })
+    }
   });
 
   /* Add data nilai. */
-  router.post('/add', function (req, res, next) {
+  router.post('/', function (req, res, next) {
     try {
-      let namaMahasiswa = req.body.Mahasiswa;
-      let namaMatkul = req.body.Matkul;
+      let idMhs = req.body.id_mhs;
+      let idMatkul = req.body.id_matkul;
       let nilaiMahasiswa = req.body.nilai;
-      let keteranganMhs = req.body.keterangan;
-      let sqlMahasiswa = `SELECT id_mhs FROM mahasiswa WHERE nama_mhs = '${namaMahasiswa}'`;
-      let sqlMatakuliah = `SELECT id_matkul FROM mata_kuliah WHERE nama_matkul = '${namaMatkul}'`;
-      pool.query(sqlMahasiswa, (err, dataMhs) => {
-        pool.query(sqlMatakuliah,(err, dataMatkul) => {
-        let sql = `INSERT INTO nilai (id_mhs, id_matkul, nilai_mhs, keterangan) VALUES (${dataMhs.rows[0].id_mhs}, ${dataMatkul.rows[0].id_matkul}, ${nilaiMahasiswa}, '${keteranganMhs}')`;
-          pool.query(sql, (err, data) => {
-          res.json({
-            message: "data has been added"
-          });
-          // console.log('ini data', data);
-          })
-        })
+      let keterangan = req.body.keterangan;
+      let sql = `INSERT INTO nilai (id_mhs, id_matkul, nilai_mhs, keterangan) VALUES (${idMhs}, ${idMatkul}, ${nilaiMahasiswa}, '${keteranganMhs}')`;
+      pool.query(sql, (err, data) => {
+        res.json({
+          success: true,
+          message: "Data has been added successfully",
+          data: data.rows
+        });
       })
     } catch (err) {
       res.json({
@@ -47,8 +98,47 @@ module.exports = function (pool) {
         message: `something went wrong : ${err.message}`
       })
     }
-
   });
+
+  /* Update nilai. */
+router.put('/:id', function (req, res, next) {
+  let id = req.params.id;
+
+  try {
+    let sql = `DELETE FROM nilai WHERE id_nilai = ${id}`;
+    pool.query(sql, (err, data) => {
+      res.json({
+        success: true,
+        message: "Data has been deleted"
+      });
+    })
+  } catch (err) {
+    res.json({
+      error: true,
+      message: `something went wrong : ${err.message}`
+    })
+  }
+});
+
+/* Delete nilai. */
+router.delete('/:id', function (req, res, next) {
+  let id = req.params.id;
+
+  try {
+    let sql = `DELETE FROM nilai WHERE id_nilai = ${id}`;
+    pool.query(sql, (err, data) => {
+      res.json({
+        success: true,
+        message: "Data has been deleted"
+      });
+    })
+  } catch (err) {
+    res.json({
+      error: true,
+      message: `something went wrong : ${err.message}`
+    })
+  }
+});
 
   return router;
 }
