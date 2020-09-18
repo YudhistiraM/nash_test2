ae863d29c35380f5bef044f51531f07c23977352
yudhies2109 <ma.yudhistira@hotmail.com>
2020-09-18 21:00:52 +0700

    store configuration

--- client/src/index.js
+++ client/src/index.js
@@ -4,23 +4,29 @@ import './index.css';
 import { Landing, Home, Add, Edit } from './components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/">
-        <Landing />
-      </Route>
-      <Route exact path="/home">
-        <Home />
-      </Route>
-      <Route exact path="/add">
-        <Add />
-      </Route>
-      <Route exact path="/edit">
-        <Edit />
-      </Route>
-    </Switch>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/add">
+          <Add />
+        </Route>
+        <Route exact path="/edit">
+          <Edit />
+        </Route>
+      </Switch>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
