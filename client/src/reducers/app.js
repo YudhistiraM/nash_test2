432d571292713b51edada3903b3b9f3def7c718e
yudhies2109 <ma.yudhistira@hotmail.com>
2020-09-21 10:35:22 +0700

    reducer config

--- client/src/reducers/app.js
+++ client/src/reducers/app.js
@@ -1,13 +1,35 @@
 
-import { LOAD_DATA_SUCCESS, LOAD_MAHASISWA_SUCCESS, LOAD_MATAKULIAH_SUCCESS } from '../constants/actionTypes.js'
+import { LOAD_DATA_SUCCESS, LOAD_MAHASISWA_SUCCESS } from '../constants/actionTypes.js'
 
-export default function data(state = [], action) {
+// export default function data(state = [], action) {
+//   switch (action.type) {
+
+//     case LOAD_DATA_SUCCESS:
+//       return action.data;
+
+//     case LOAD_MAHASISWA_SUCCESS:
+//       return action.data;
+
+//     case LOAD_MATAKULIAH_SUCCESS:
+//       return action.data;
+
+//     default:
+//       return state;
+//   }
+// }
+
+const data = (state = [], action) => {
   switch (action.type) {
 
     case LOAD_DATA_SUCCESS:
+      return action.data;
+
+    case LOAD_MAHASISWA_SUCCESS:
       return action.data;
 
     default:
       return state;
   }
 }
+
+export default data;
